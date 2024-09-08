6fd0c92c9902dcb9c1e2ad8e5ab3986f5849bc2c
T3RRYT3RR0R <97126967+T3RRYT3RR0R@users.noreply.github.com>
2024-09-08 13:35:22 +0800

    Delete Examples directory

--- Examples/waveDemo.bat
+++
@@ -1,8 +0,0 @@
-@echo off & setlocal enableDelayedExpansion &CLS
-
-Call "%~dp0automacro.bat" @waveprint
-
-Set output=%username% %computername% %date% %time%
-
-%@waveprint% output 1 10 9 38;2;;;250
-echo(!$waveprint!!\E![!y.lBound!d!\E![0m!\E![E
